cd536e07a533935f1e564d5162c60bb854130628
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-11-12 10:59:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinPonavljanjeZadatak5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int karte;
+            int zbroj = 0;
+            while(zbroj<31)
+            {
+               karte=Convert.ToInt32( Console.ReadLine());
+                if (karte > 0 && karte < 14)
+                {
+                    zbroj = zbroj + karte;
+                }
+            }
+            if (zbroj == 31)
+            {
+                Console.WriteLine("Pobijedio si");
+            }
+            else
+            {
+                Console.WriteLine("Izgubio si");
+            }
+            Console.ReadKey();
+        }
+      
+    }
+}
